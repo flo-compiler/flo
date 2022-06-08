1ee8d47677a5de19995d6b49ef27b8b306b4fe90
vanelk <ditrix.360@gmail.com>
2022-06-08 17:55:29 -0400

    fixed context bug

--- src/compiler.py
+++ src/compiler.py
@@ -186,7 +186,6 @@ class Compiler(Visitor):
                         node.method_body.is_variadic, self.class_within)
         fn.class_within = self.class_within
         self.class_within.add_method(fn)
-        self.context.set(method_name, fn)
         self.evaluate_function_body(fn, arg_names, node.method_body.body)
 
 
@@ -303,7 +302,6 @@ class Compiler(Visitor):
 
     def visitFncCallNode(self, node: FncCallNode):
         fnc = self.visit(node.name)
-        print(node.name.var_name)
         args = [self.visit(arg) for arg in node.args]
         return fnc.call(self.builder, args)
 
