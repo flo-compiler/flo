1a097fe85f83a6ef0862472e4ec5e652bdecb9d0
vanelk <ditrix.360@gmail.com>
2022-06-22 11:11:26 -0400

    compile time stats

--- compile_selfhost.sh
+++ compile_selfhost.sh
@@ -1,3 +1,11 @@
-./src/flo.py selfhost/flo.flo
+start=`date +%s`
+./src/flo.py selfhost/flo.flo --opt-level=3
 gcc flo.o -o flo
 rm flo.o
+end=`date +%s`
+echo "Compiled Flo Compiler!"
+runtime=$(($end - $start))
+echo "Compile time: "$runtime"s"
+
+echo "File size:"
+du -sh flo
