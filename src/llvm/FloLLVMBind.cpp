4a0d0f4995244aa4cf3e4e9358f53759aa1f2783
vanelk <ditrix.360@gmail.com>
2023-01-25 19:29:18 -0500

    - Fix Godegen VarAccess. Lead to wrong paramter types for function calls and all types of isues. - Makefile changed to reflect change in additinal bindings to c++. - New Test case for That issue.

---
+++ src/llvm/FloLLVMBind.cpp
@@ -0,0 +1,40 @@
+#include <llvm/IR/Function.h>
+#include <llvm/Support/TargetSelect.h>
+extern "C" llvm::FunctionType* LLVM_GetFunctionType(llvm::Function* func){
+    return func->getFunctionType();
+}
+extern "C" void LLVM_InitializeAllTargetInfos (){
+    return llvm::InitializeAllTargetInfos();
+}
+extern "C" void LLVM_InitializeAllTargets (){
+    return llvm::InitializeAllTargets();
+}
+
+extern "C" void LLVM_InitializeAllTargetMCs (){
+    return llvm::InitializeAllTargetMCs();
+}
+
+extern "C" void LLVM_InitializeAllAsmPrinters (){
+    return llvm::InitializeAllAsmPrinters();
+}
+
+extern "C" void LLVM_InitializeAllAsmParsers (){
+    return llvm::InitializeAllAsmParsers();
+}
+
+extern "C" void LLVM_InitializeAllDisassemblers(){
+    return llvm::InitializeAllDisassemblers();
+}
+extern "C" int LLVM_InitializeNativeTarget(){
+    return llvm::InitializeNativeTarget();
+}
+
+extern "C" int LLVM_InitializeNativeAsmParser(){
+    return llvm::InitializeNativeTargetAsmParser();
+}
+extern "C" int LLVM_InitializeNativeAsmPrinter(){
+    return llvm::InitializeNativeTargetAsmPrinter();
+}
+extern "C" int LLVM_InitializeNativeDisassembler(){
+    return llvm::InitializeNativeTargetDisassembler();
+}
