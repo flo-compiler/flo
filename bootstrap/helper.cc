2ae6e67d00b38000ce77d14bf5f239b6cb4712a0
vanelk <ditrix.360@gmail.com>
2023-07-04 13:35:05 -0400

    - using c++ for bootstrap process

---
+++ bootstrap/helper.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <llvm/IR/Module.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/Host.h>
+#include <llvm/Support/CodeGen.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Target/TargetOptions.h>
+#include <llvm/Target/TargetMachine.h>
+#include <llvm/MC/TargetRegistry.h>
+#include <llvm/ADT/Optional.h>
+int main(int argc, char** argv){
+    llvm::SMDiagnostic Err;
+    llvm::LLVMContext ctx;
+    std::unique_ptr<llvm::Module> M = parseIRFile(argv[1], Err, ctx);
+
+    llvm::InitializeAllTargetMCs();
+    llvm::InitializeAllTargets();
+    llvm::InitializeAllTargetInfos();
+    llvm::InitializeAllAsmPrinters();
+
+    auto TargetTriple = llvm::sys::getDefaultTargetTriple();
+    std::string Error;
+    auto Target = llvm::TargetRegistry::lookupTarget(TargetTriple, Error);
+    if (!Target) {
+        std::cerr << Error;
+        return 1;
+    }
+    auto CPU = "generic";
+    auto Features = "";
+    llvm::TargetOptions opt;
+    auto RM = llvm::Optional<llvm::Reloc::Model>();
+    auto TargetMachine = Target->createTargetMachine(TargetTriple, CPU, Features, opt, RM);
+
+    M->setDataLayout(TargetMachine->createDataLayout());
+    M->setTargetTriple(TargetTriple);
+
+    std::error_code EC;
+    llvm::raw_fd_ostream dest(argv[2], EC, llvm::sys::fs::OF_None);
+
+    if (EC) {
+        std::cerr << "Could not open file: " << EC.message();
+        return 1;
+    }
+
+    llvm::legacy::PassManager pass;
+    auto FileType = llvm::CGFT_ObjectFile;
+
+    if (TargetMachine->addPassesToEmitFile(pass, dest, nullptr, FileType)) {
+        std::cerr << "TargetMachine can't emit a file of this type";
+        return 1;
+    }
+
+    pass.run(*M);
+    dest.flush();
+}
